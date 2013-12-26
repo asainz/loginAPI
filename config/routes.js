a96126a8e6433100c6ee3711398de9a3d22e9d8f
Andres Sainz de Aja <andres.sainz@globant.com>
2013-12-25 21:30:59 -0300

    update routing configuration

--- config/routes.js
+++ config/routes.js
@@ -1,10 +1,17 @@
 module.exports = function(app){
-
-	//home route
-	// var home = require('../app/controllers/home');
-	// app.get('/', home.index);
-
     var user = require('../app/controllers/user');
+
     app.get('/', user.createProfile);
 
+    app.get('/login', user.login);
+    app.post('/login', user.doLogin);
+
+    app.get('/register', user.register);
+    app.post('/register', user.doRegister);
+
+    app.get('/profile', user.profile);
+    app.put('/profile', user.updateProfile);
+    app.delete('/profile', user.deleteProfile);
+
+    app.post('/logout', user.doLogout);
 };
